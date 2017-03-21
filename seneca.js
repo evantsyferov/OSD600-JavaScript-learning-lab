b9322a4452526d96bee6b4373461690c0bfd5c0b
Genya Antsyferov <Genya Antsyferov>
2017-03-21 09:51:14 -0400

    added code for email and name functions

--- seneca.js
+++ seneca.js
@@ -1,1 +1,22 @@
+// [INFO] See discussion of node.js exports here:
+// https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 
+/**
+ * Given a string `email`, return `true` if the string is in the form
+ * of a valid Seneca College email address, `false` othewise.
+ */
+exports.isValidEmail = function(email) {
+    if(email==/[A-Za-z]@myseneca.ca/)
+        return true;
+    else
+        retrn false;
+};
+
+/**
+ * Given a string `name`, return a formatted Seneca email address for
+ * this person. NOTE: the email doesn't need to be real/valid/active.
+ */
+exports.formatSenecaEmail = function(name) {
+    var email= name + "@myseneca.ca";
+    return email;
+};
