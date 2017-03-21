d340d537c5912750003b0483e988a6e070b36aa1
Genya Antsyferov <Genya Antsyferov>
2017-03-21 12:30:40 -0400

    fixing eslint errors

--- seneca.js
+++ seneca.js
@@ -5,18 +5,19 @@
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
  */
-exports.isValidEmail = function(email) {
-    if(email==/[A-Za-z]@myseneca.ca/)
-        return true;
-    else
-        return false;
+exports.isValidEmail = function (email) {
+  let emailval = false;
+  if (email === /[A-Za-z]@myseneca.ca/) {
+    emailval = true;
+  }
+  return emailval;
 };
 
 /**
  * Given a string `name`, return a formatted Seneca email address for
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
-exports.formatSenecaEmail = function(name) {
-    var email= name + "@myseneca.ca";
-    return email;
+exports.formatSenecaEmail = function (name) {
+  const email = name.concat('@myseneca.ca');
+  return email;
 };
